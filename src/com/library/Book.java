643c6054bc77e64ae8696d5b6aa8f3d677af1f11
Tapabrata Bakuli <tapabrata.bakuli@gmail.com>
2026-03-28 12:00:00 +0530

    Add Book model class

---
+++ src/com/library/Book.java
@@ -0,0 +1,40 @@
+package com.library;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s by %s - %s", isbn, title, author, isAvailable ? "Available" : "Borrowed");
+    }
+}
